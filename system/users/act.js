f124a5703a1917b9b8302304da0fd71b7bc533c4
Leonid Jakimovski <ljakimovski211@yahoo.com>
2019-12-19 12:56:11 +0100

    error fixed in users/act.js

--- system/users/act.js
+++ system/users/act.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const helper = require('../helper/helper');
 
 
-getAllUsers = async(req, res) => {
+getAllUsers = async (req, res) => {
     const userId = req.params.userId;
 
     const users = await queries.getAllUsersQuery();
@@ -13,11 +13,11 @@ getAllUsers = async(req, res) => {
         return userId == user.Id
     })
 
-    if(userExist == true){
+    if (userExist == true) {
         const userTypes = await queries.adminGetOneUserQuery(userId);
         const checkUserType = userTypes[0].User_type;
-    
-        if(checkUserType == 'client'){
+
+        if (checkUserType == 'client') {
             res.status(400).json({
                 success: false,
                 message: 'You don\'t have permissions to do that.'
@@ -38,36 +38,34 @@ getAllUsers = async(req, res) => {
             message: `Admin with ID of ${userId}, does not exist.`
         });
     }
-    
+
 };
 
-getUserInfoAndEvent = async(req, res) => {
+getUserInfoAndEvent = async (req, res) => {
     const userId = req.params.userId;
 
     const users = await getAllUsersQuery();
     const userExist = users.some(user => {
         return userId == user.Id
     });
-    
+
     const events = await evnetsQueries.getAllEventsQuery();
     const eventExist = events.some(event => {
         return userId == event.User_id
     });
     // console.log(eventExist);
 
-    if(userExist == false) {
+    if (userExist == false) {
         res.status(400).json({
             success: false,
             message: `User with ID of ${userId}, does not exist.`
         });
-    } 
-    else if(eventExist == false) {
+    } else if (eventExist == false) {
         res.status(400).json({
             success: false,
             message: `User with ID if ${userId}, does not have any events.`
         });
-    } 
-    else {
+    } else {
         try {
             const userInfoAndEvents = await queries.getUserInfoAndEventsQuery(userId);
             const events = userInfoAndEvents.map(events => {
@@ -79,21 +77,21 @@ getUserInfoAndEvent = async(req, res) => {
                 return eventsObj
             });
             res.status(200).json({
-                userInfo:{
+                userInfo: {
                     name: userInfoAndEvents[0].Name,
                     lastName: userInfoAndEvents[0].Lastname,
                     email: userInfoAndEvents[0].Email
                 },
                 events
             });
-            
+
         } catch (error) {
             res.status(500).send(error.message);
         }
     }
 };
 
-getMyProfile = async(req, res) => {
+getMyProfile = async (req, res) => {
     const userId = req.params.userId;
 
     const users = await queries.getAllUsersQuery();
@@ -101,7 +99,7 @@ getMyProfile = async(req, res) => {
         return userId == user.Id
     });
 
-    if(userExist == false) {
+    if (userExist == false) {
         res.status(400).json({
             success: false,
             message: `User tiwh ID of ${userId}, does not exist.`
@@ -119,7 +117,7 @@ getMyProfile = async(req, res) => {
 };
 
 
-signUp = async(req, res) => {
+signUp = async (req, res) => {
     const userRequest = req.body;
     const firstname = req.body.Firstname;
     const lastname = req.body.Lastname;
@@ -139,53 +137,46 @@ signUp = async(req, res) => {
         return username === user.Username
     });
 
-    if(firstname == null || lastname == null || username == null || email == null || password == null){
+    if (firstname == null || lastname == null || username == null || email == null || password == null) {
         res.status(400).json({
             success: false,
             message: 'All fiels need to be filled with information.'
         });
-    } 
-    else if(usernameExists){
+    } else if (usernameExists) {
         res.status(400).json({
             success: false,
             message: `Username ${username}, is already in use. Try with another one.`
         });
-    } 
-    else if(username.length < 4){
+    } else if (username.length < 4) {
         res.status(400).json({
             success: false,
             message: `Username ${username}, is not valid. Your username must contain at leas 4 characters.`
         });
-    }
-    else if(emailExist){
+    } else if (emailExist) {
         res.status(400).json({
             success: false,
             message: `Email ${email}, is already in use. Try with another one.`
         });
-    } 
-    else if(helper.symbols.test(email) == false) {
+    } else if (helper.symbols.test(email) == false) {
         return res.status(400).json({
             success: false,
             message: `Email ${email}, is not valid. Try with another one.`
         });
-    } 
-    else if(password.length <= 8) {
+    } else if (password.length <= 8) {
         res.status(400).json({
             success: false,
             message: 'Password should contain at least 6 characters.'
         });
-    } 
-    else if(helper.passwordTest.test(password) == false) {
+    } else if (helper.passwordTest.test(password) == false) {
         return res.status(400).json({
             success: false,
             message: 'Your password must contain 1 lower case letter, 1 capital letter, 1 or more numbers and a special character as !@#$%^&*'
         });
-    } 
-     else {
+    } else {
         try {
             const passHash = bcrypt.hashSync(userRequest.Password, 10);
             await queries.signUpQuery(firstname, lastname, username, email, companyName, userType, passHash);
-            
+
             res.status(201).json({
                 message: 'User has been created!'
             });
@@ -196,7 +187,7 @@ signUp = async(req, res) => {
 };
 
 
-logIn = async(req, res) => {
+logIn = async (req, res) => {
     const username = req.body.Username;
     // const email = req.body.Email;
     const password = req.body.Password;
@@ -206,38 +197,36 @@ logIn = async(req, res) => {
         return username === user.Username
     });
 
-    if(username == null || password == null){
+    if (username == null || password == null) {
         res.status(400).json({
             success: false,
             message: 'Please nter your username and password.'
         });
-    } 
-    else if(usernameExists == false){
+    } else if (usernameExists == false) {
         res.status(400).json({
             success: false,
             message: `Username ${username}, does not exist. Try with another one.`
         });
-    } 
-    else if(username.length <= 4) {
+    } else if (username.length <= 4) {
         res.status(400).json({
             success: false,
             message: `Username ${username}, is not valid. Username should contain at least 4 characters.`
         });
-    } 
-    else if(password.length <= 1){
+    } else if (password.length <= 1) {
         res.status(400).json({
             success: false,
             message: 'Password is not valid.'
         });
-    } 
-    else {
+    } else {
         try {
             const user = await queries.logInUserQuery(username);
             const newUser = user[0];
             const matchPassword = bcrypt.compareSync(password, newUser.Password);
-    
-            if(matchPassword){
-                jwt.sign({user: newUser}, 'secret', (err, token)=>{
+
+            if (matchPassword) {
+                jwt.sign({
+                    user: newUser
+                }, 'secret', (err, token) => {
                     res.json({
                         token,
                         message: 'You have been logged in.'
@@ -257,7 +246,7 @@ logIn = async(req, res) => {
 
 
 
-editMyProfile = async(req, res) => {
+editMyProfile = async (req, res) => {
     const userId = req.params.userId;
 
     const name = req.body.Name;
@@ -265,7 +254,7 @@ editMyProfile = async(req, res) => {
     const username = req.body.Username;
     const email = req.body.Email;
     const companyName = req.body.Company_name;
-    
+
 
     const users = await queries.getAllUsersQuery();
     const userExist = users.some(user => {
@@ -273,25 +262,25 @@ editMyProfile = async(req, res) => {
     });
 
     const uneditedUsername = users.filter(user => {
-        if(username == null) {
+        if (username == null) {
             username == user.Username
         } else {
             user.Username = username
         }
 
-        if(name == null) {
+        if (name == null) {
             name == user.Name
         } else {
             user.Name = name
         }
 
-        if(email == null){
+        if (email == null) {
             email == user.Email
         } else {
             user.Email = email
         }
 
-        if(lastname == null){
+        if (lastname == null) {
             lastName == user.Lastname
         } else {
             user.Lastname = lastname
@@ -301,16 +290,15 @@ editMyProfile = async(req, res) => {
 
     const finalResults = uneditedUsername[0];
 
-    if(userExist == false) {
+    if (userExist == false) {
         res.status(400).json({
             success: false,
             message: `User with ID if ${userId}, does not exist.`
         });
-    } 
-    else {
+    } else {
         try {
             await queries.editMyProfileQuery(finalResults.Name, finalResults.Lastname, finalResults.Username, finalResults.Email, companyName, userId);
-    
+
             res.status(200).json({
                 message: `User with ID of ${userId}, has been updated.`
             });
@@ -322,7 +310,7 @@ editMyProfile = async(req, res) => {
 
 // ADMIN
 
-adminDeleteUserProfile = async(req, res) => {
+adminDeleteUserProfile = async (req, res) => {
     const userId = req.params.userId;
     const adminId = req.params.adminId;
 
@@ -338,7 +326,7 @@ adminDeleteUserProfile = async(req, res) => {
         return adminId == user.Id
     })
 
-    if(adminExist == false){
+    if (adminExist == false) {
         res.status(400).json({
             success: false,
             message: `Admin with ID of ${adminId}, does not exist.`
@@ -348,17 +336,15 @@ adminDeleteUserProfile = async(req, res) => {
             success: false,
             message: `User with ID of ${userId}, does not exist.`
         });
-    }
-    else if(checkUserType == 'client'){
+    } else if (checkUserType == 'client') {
         res.status(400).json({
             success: false,
             message: 'You don\'t have permissions to do that.'
         });
-    } 
-     else {
+    } else {
         try {
             await queries.adminDeleteUserProfileQuery(userId);
-    
+
             res.status(200).json({
                 message: `User with ID of ${userId}, has been deleted.`
             });
@@ -369,7 +355,7 @@ adminDeleteUserProfile = async(req, res) => {
 };
 
 
-adminGetOneUser = async(req, res) => {
+adminGetOneUser = async (req, res) => {
     const userId = req.params.userId;
     const adminId = req.params.adminId;
 
@@ -381,13 +367,12 @@ adminGetOneUser = async(req, res) => {
         return userId == user.Id
     });
 
-    if(checkUserType == 'client'){
+    if (checkUserType == 'client') {
         res.status(400).json({
             success: false,
             message: `User with ID of ${adminId}, does not have permissions to do that.`
         });
-    } 
-    else if(userExist == false){
+    } else if (userExist == false) {
         res.status(400).json({
             success: false,
             message: `User with ID of ${userId}, does not exist.`
